853cc3a953a5a5ca7d7f41fc416bc5595b5d57dc
Carlos Palma <carpalsan1@alum.us.es>
2025-11-09 13:50:31 +0100

    fix: lint forms

--- app/modules/community/forms.py
+++ app/modules/community/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length, Optional
 
+
 class CommunityForm(FlaskForm):
     name = StringField(
         'Name',
@@ -10,15 +11,15 @@ class CommunityForm(FlaskForm):
             Length(min=3, max=100, message='Name must be between 3 and 100 characters')
         ]
     )
-    
+
     description = TextAreaField(
         'Description',
         validators=[Optional()]
     )
-    
+
     logo = StringField(
         'Logo URL',
         validators=[Optional(), Length(max=255)]
     )
-    
+
     submit = SubmitField('Create Community')
